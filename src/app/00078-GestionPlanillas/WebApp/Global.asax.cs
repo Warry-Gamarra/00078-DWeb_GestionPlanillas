8e6be3c4159c45f0ae1078b274ea05c5076a55a9
wgamarra <wgamarra2310@gmail.com>
2021-12-28 17:11:56 -0500

    estructura del proyecto y configuracion inicial

---
+++ src/app/00078-GestionPlanillas/WebApp/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using WebMatrix.WebData;
+
+namespace WebApp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            if (!WebSecurity.Initialized)
+            {
+                WebSecurity.InitializeDatabaseConnection("BD_GestionPlanillasConnection", "TC_Usuario", "UserId", "UserName", autoCreateTables: false);
+                if (Roles.Enabled)
+                {
+                    if (!Roles.RoleExists("Administrador"))
+                        Roles.CreateRole("Administrador");
+
+                    if (!Roles.RoleExists("Consulta"))
+                        Roles.CreateRole("Consulta");
+
+                    if (!Roles.RoleExists("Contabilidad"))
+                        Roles.CreateRole("Contabilidad");
+
+                    if (!Roles.RoleExists("Tesorería"))
+                        Roles.CreateRole("Tesorería");
+
+                    if (!Roles.RoleExists("Dependencia"))
+                        Roles.CreateRole("Dependencia");
+
+                }
+            }
+
+        }
+    }
+}
