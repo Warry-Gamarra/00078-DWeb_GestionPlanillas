8d6d24808876c4de384bbf1b3fbea2a98f4e2b14
hmanyari <manyari_571@hotmail.com>
2023-01-26 17:01:36 -0500

    .

--- src/database/00078_GestionPlanillas/BD_OCRH_GestionPlanillas/2. BD_OCRH_GestionPlanillas - Esquema de base de datos.sql
+++ src/database/00078_GestionPlanillas/BD_OCRH_GestionPlanillas/2. BD_OCRH_GestionPlanillas - Esquema de base de datos.sql
@@ -113,3 +113,233 @@ CREATE TABLE TI_UsuarioDatosUsuario
 )
 go
 
+
+/*	-----------------------  Negocio	-----------------------  */
+
+
+CREATE TABLE TC_TipoPlanilla
+(
+	I_TipoPlanillaID INT IDENTITY(1,1),
+	C_TipoPlanillaCod VARCHAR(20) NOT NULL,
+	T_TipoPlanillaDesc VARCHAR(250) NOT NULL,
+	B_Habilitado BIT NOT NULL,
+	B_Eliminado BIT NOT NULL,
+	I_UsuarioCre INT,
+	D_FecCre DATETIME,
+	I_UsuarioMod INT,
+	D_FecMod DATETIME,
+	CONSTRAINT PK_TipoPlanilla PRIMARY KEY (I_TipoPlanillaID)
+)
+
+CREATE TABLE TC_ClasePlanilla
+(
+	I_ClasePlanillaID INT IDENTITY(1,1),
+	I_TipoPlanillaID INT NOT NULL,
+	C_ClasePlanillaCod VARCHAR(20) NOT NULL,
+	T_ClasePlanillaDesc VARCHAR(250) NOT NULL,
+	B_Habilitado BIT NOT NULL,
+	B_Eliminado BIT NOT NULL,
+	I_UsuarioCre INT,
+	D_FecCre DATETIME,
+	I_UsuarioMod INT,
+	D_FecMod DATETIME,
+	CONSTRAINT PK_ClasePlanilla PRIMARY KEY (I_ClasePlanillaID),
+	CONSTRAINT FK_TipoPlanilla_ClasePlanilla FOREIGN KEY (I_TipoPlanillaID) REFERENCES TC_TipoPlanilla(I_TipoPlanillaID)
+)
+
+CREATE TABLE TC_TipoConcepto
+(
+	I_TipoConceptoID INT IDENTITY(1,1),
+	T_TipoConceptoDesc VARCHAR(250) NOT NULL,
+	B_Habilitado BIT NOT NULL,
+	B_Eliminado BIT NOT NULL,
+	I_UsuarioCre INT,
+	D_FecCre DATETIME,
+	I_UsuarioMod INT,
+	D_FecMod DATETIME,
+	CONSTRAINT PK_TipoConcepto PRIMARY KEY (I_TipoConceptoID)
+)
+
+CREATE TABLE TC_Concepto
+(
+	I_ConceptoID INT IDENTITY(1,1),
+	I_TipoConceptoID INT NOT NULL,
+	T_ConceptoDesc VARCHAR(250) NOT NULL,
+	M_Valor DECIMAL(15,2),
+	B_EsFijo BIT,
+	B_Habilitado BIT NOT NULL,
+	B_Eliminado BIT NOT NULL,
+	I_UsuarioCre INT,
+	D_FecCre DATETIME,
+	I_UsuarioMod INT,
+	D_FecMod DATETIME,
+	CONSTRAINT PK_Concepto PRIMARY KEY (I_ConceptoID),
+	CONSTRAINT FK_TipoConcepto_Concepto FOREIGN KEY (I_TipoConceptoID) REFERENCES TC_TipoConcepto(I_TipoConceptoID)
+)
+
+CREATE TABLE TI_PlantillaPlanilla
+(
+	I_PlantillaPlanillaID INT IDENTITY(1,1),
+	I_ClasePlanillaID INT NOT NULL,
+	B_Habilitado BIT NOT NULL,
+	B_Eliminado BIT NOT NULL,
+	I_UsuarioCre INT,
+	D_FecCre DATETIME,
+	I_UsuarioMod INT,
+	D_FecMod DATETIME,
+	CONSTRAINT PK_PlantillaPlanilla PRIMARY KEY (I_PlantillaPlanillaID),
+	CONSTRAINT FK_ClasePlanilla_PlantillaPlanilla FOREIGN KEY (I_ClasePlanillaID) REFERENCES TC_ClasePlanilla(I_ClasePlanillaID)
+)
+
+CREATE TABLE TI_PlantillaPlanilla_Concepto
+(
+	I_PlantillaPlanillaConceptoID INT IDENTITY(1,1),
+	I_PlantillaPlanillaID INT NOT NULL,
+	I_ConceptoID INT NOT NULL,
+	B_Habilitado BIT NOT NULL,
+	B_Eliminado BIT NOT NULL,
+	I_UsuarioCre INT,
+	D_FecCre DATETIME,
+	I_UsuarioMod INT,
+	D_FecMod DATETIME,
+	CONSTRAINT PK_PlantillaPlanillaConcepto PRIMARY KEY (I_PlantillaPlanillaConceptoID),
+	CONSTRAINT FK_PlantillaPlanilla_PlantillaPlanillaConcepto FOREIGN KEY (I_PlantillaPlanillaID) REFERENCES TI_PlantillaPlanilla(I_PlantillaPlanillaID),
+	CONSTRAINT FK_Concepto_PlantillaPlanillaConcepto FOREIGN KEY (I_ConceptoID) REFERENCES TC_Concepto(I_ConceptoID)
+)
+
+CREATE TABLE TC_TipoDocumento
+(
+	I_TipoDocumentoID INT IDENTITY(1,1),
+	T_TipoDocumentoDesc VARCHAR(250),
+	B_Habilitado BIT NOT NULL,
+	B_Eliminado BIT NOT NULL,
+	I_UsuarioCre INT,
+	D_FecCre DATETIME,
+	I_UsuarioMod INT,
+	D_FecMod DATETIME,
+	CONSTRAINT PK_TipoDocumento PRIMARY KEY (I_TipoDocumentoID)
+)
+
+CREATE TABLE TC_Persona
+(
+	I_PersonaID INT IDENTITY(1,1),
+	I_TipoDocumentoID INT,
+	C_NumDocumento VARCHAR(20),
+	T_Nombre VARCHAR(250) NOT NULL,
+	T_ApellidoPaterno VARCHAR(250),
+	T_ApellidoMaterno VARCHAR(250),
+	B_Habilitado BIT NOT NULL,
+	B_Eliminado BIT NOT NULL,
+	I_UsuarioCre INT,
+	D_FecCre DATETIME,
+	I_UsuarioMod INT,
+	D_FecMod DATETIME,
+	CONSTRAINT PK_Persona PRIMARY KEY (I_PersonaID),
+	CONSTRAINT FK_TipoDocumento_Persona FOREIGN KEY (I_TipoDocumentoID) REFERENCES TC_TipoDocumento(I_TipoDocumentoID)
+)
+
+CREATE TABLE TC_Vinculo
+(
+	I_VinculoID INT IDENTITY(1,1),
+	T_VinculoDesc VARCHAR(250) NOT NULL,
+	B_Habilitado BIT NOT NULL,
+	B_Eliminado BIT NOT NULL,
+	I_UsuarioCre INT,
+	D_FecCre DATETIME,
+	I_UsuarioMod INT,
+	D_FecMod DATETIME,
+	CONSTRAINT PK_Vinculo PRIMARY KEY (I_VinculoID)
+)
+
+CREATE TABLE TC_Trabajador
+(
+	I_TrabajadorID INT IDENTITY(1,1),
+	I_PersonaID INT NOT NULL,
+	D_FechaIngreso DATE,
+	X_Regimen VARCHAR(250),
+	X_Dependencia VARCHAR(250),
+	I_ClasePlanillaID INT NOT NULL,
+	I_VinculoID INT NOT NULL,
+	B_Habilitado BIT NOT NULL,
+	B_Eliminado BIT NOT NULL,
+	I_UsuarioCre INT,
+	D_FecCre DATETIME,
+	I_UsuarioMod INT,
+	D_FecMod DATETIME,
+	CONSTRAINT PK_Trabajador PRIMARY KEY (I_TrabajadorID),
+	CONSTRAINT FK_Persona_Trabajador FOREIGN KEY (I_PersonaID) REFERENCES TC_Persona(I_PersonaID),
+	CONSTRAINT FK_ClasePlanilla_Trabajador FOREIGN KEY (I_ClasePlanillaID) REFERENCES TC_ClasePlanilla(I_ClasePlanillaID),
+	CONSTRAINT FK_Vinculo_Trabajador FOREIGN KEY (I_VinculoID) REFERENCES TC_Vinculo(I_VinculoID),
+)
+
+CREATE TABLE TR_Periodo
+(
+	I_PeriodoID INT IDENTITY(1,1),
+	I_Anio INT NOT NULL,
+	I_Mes INT NOT NULL,
+	T_MesDesc VARCHAR(20) NOT NULL,
+	I_UsuarioCre INT,
+	D_FecCre DATETIME,
+	I_UsuarioMod INT,
+	D_FecMod DATETIME,
+	CONSTRAINT PK_Periodo PRIMARY KEY (I_PeriodoID)
+)
+
+CREATE TABLE TR_Planilla
+(
+	I_PlanillaID INT IDENTITY(1,1),
+	I_PeriodoID INT NOT NULL,
+	I_ClasePlanillaID INT NOT NULL,
+	I_Correlativo INT NOT NULL,
+	I_CantRegistros INT NOT NULL,
+	I_TotalRemuneracion DECIMAL(15,2) NOT NULL,
+	I_TotalDescuento DECIMAL(15,2) NOT NULL,
+	I_TotalReintegro DECIMAL(15,2) NOT NULL,
+	I_TotalDeduccion DECIMAL(15,2) NOT NULL,
+	I_TotalSueldo DECIMAL(15,2) NOT NULL,
+	B_Anulado BIT NOT NULL,
+	I_UsuarioCre INT,
+	D_FecCre DATETIME,
+	I_UsuarioMod INT,
+	D_FecMod DATETIME,
+	CONSTRAINT PK_Planilla PRIMARY KEY (I_PlanillaID),
+	CONSTRAINT FK_Periodo_Planilla FOREIGN KEY (I_PeriodoID) REFERENCES TR_Periodo(I_PeriodoID),
+	CONSTRAINT FK_ClasePlanilla_Planilla FOREIGN KEY (I_ClasePlanillaID) REFERENCES TC_ClasePlanilla(I_ClasePlanillaID)
+)
+
+CREATE TABLE TR_TrabajadorPlanilla
+(
+	I_TrabajadorPlanillaID INT IDENTITY(1,1),
+	I_PlanillaID INT NOT NULL,
+	I_TrabajadorID INT NOT NULL,
+	I_TotalRemuneracion DECIMAL(15,2) NOT NULL,
+	I_TotalDescuento DECIMAL(15,2) NOT NULL,
+	I_TotalReintegro DECIMAL(15,2) NOT NULL,
+	I_TotalDeduccion DECIMAL(15,2) NOT NULL,
+	I_TotalSueldo DECIMAL(15,2) NOT NULL,
+	B_Anulado BIT NOT NULL,
+	I_UsuarioCre INT,
+	D_FecCre DATETIME,
+	I_UsuarioMod INT,
+	D_FecMod DATETIME,
+	CONSTRAINT PK_TrabajadorPlanilla PRIMARY KEY (I_TrabajadorPlanillaID),
+	CONSTRAINT FK_Planilla_TrabajadorPlanilla FOREIGN KEY (I_PlanillaID) REFERENCES TR_Planilla(I_PlanillaID),
+	CONSTRAINT FK_Trabajador_TrabajadorPlanilla FOREIGN KEY (I_TrabajadorID) REFERENCES TC_Trabajador(I_TrabajadorID)
+)
+
+CREATE TABLE TR_Concepto_TrabajadorPlanilla
+(
+	I_ConceptoTrabajadorPlanilla INT IDENTITY(1,1),
+	I_TrabajadorPlanillaID INT NOT NULL,
+	I_ConceptoID INT NOT NULL,
+	T_ConceptoDesc VARCHAR(250) NOT NULL,
+	M_Monto DECIMAL(15,2) NOT NULL,
+	B_Anulado BIT NOT NULL,
+	I_UsuarioCre INT,
+	D_FecCre DATETIME,
+	I_UsuarioMod INT,
+	D_FecMod DATETIME,
+	CONSTRAINT PK_ConceptoTrabajadorPlanilla PRIMARY KEY (I_ConceptoTrabajadorPlanilla),
+	CONSTRAINT FK_Planilla_ConceptoTrabajadorPlanilla FOREIGN KEY (I_TrabajadorPlanillaID) REFERENCES TR_TrabajadorPlanilla(I_TrabajadorPlanillaID),
+	CONSTRAINT FK_Concepto_ConceptoTrabajadorPlanilla FOREIGN KEY (I_ConceptoID) REFERENCES TC_Concepto(I_ConceptoID)
+)
