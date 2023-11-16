4b6cea6ea3831f14ab02f3bdddae14253c431ef1
Humberto Manyari <manyari_571@hotmail.com>
2023-11-16 11:47:36 -0500

    Agregado reporte de actividad y dependencia en Excel

--- src/app/00078-GestionPlanillas/Domain/Services/Implementations/GeneracionArchivoExcelService.cs
+++ src/app/00078-GestionPlanillas/Domain/Services/Implementations/GeneracionArchivoExcelService.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using Formats = Domain.Helpers.Formats;
 
 namespace Domain.Services.Implementations
 {
@@ -97,37 +98,83 @@ namespace Domain.Services.Implementations
             using (var workbook = new XLWorkbook())
             {
                 var worksheet = workbook.Worksheets.Add("Hoja1");
+                worksheet.PageSetup.PaperSize = XLPaperSize.A4Paper;
+                worksheet.PageSetup.PageOrientation = XLPageOrientation.Landscape;
+                worksheet.PageSetup.Margins.Left = 0.25;
+                worksheet.PageSetup.Margins.Right = 0.25;
+                worksheet.PageSetup.Margins.Top = 0.25;
+                worksheet.PageSetup.Margins.Bottom = 0.25;
+                worksheet.PageSetup.Scale = 85;
 
                 worksheet.Column("C").Width = 60;
+                worksheet.Columns("D:I").Width = 14;
 
                 var image = worksheet.AddPicture(HttpContext.Current.Server.MapPath("~\\Assets\\images\\logo.png"));
+                image.MoveTo(worksheet.Cell(1, 1), worksheet.Cell(3, 3));
 
-                image.MoveTo(worksheet.Cell(1, 1), worksheet.Cell(2, 2));
+                var celdaFecha = worksheet.Cell(1, 9).SetValue<string>(reporte.fechaConsulta);
+                celdaFecha.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Right);
 
-                worksheet.Cell(1, 9).SetValue<string>(reporte.fechaConsulta);
-                worksheet.Cell(2, 9).SetValue<string>(reporte.horaConsulta);
+                var celdaHora = worksheet.Cell(2, 9).SetValue<string>(reporte.horaConsulta);
+                celdaHora.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Right);
+
                 worksheet.Cell(4, 1).SetValue<string>(reporte.oficina);
                 worksheet.Cell(5, 1).SetValue<string>(reporte.subOficina);
 
-                var titleCell = worksheet.Cell(6, 1);
-
-                titleCell.Value = reporte.titulo;
-
-                worksheet.Range(titleCell, worksheet.Cell(6, 9)).Merge(true);
+                var celdaTitulo = worksheet.Cell(6, 1).SetValue<string>(reporte.titulo);
+                celdaTitulo.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+                celdaTitulo.Style.Font.Bold = true;
+                
+                worksheet.Range(celdaTitulo, worksheet.Cell(6, 9)).Merge(true);
 
                 currentRow = 8;
 
                 foreach (var actividad in reporte.listaResumenPorActividad)
                 {
-                    worksheet.Cell(currentRow, 1).SetValue<string>("Actividad");
+                    var celdaActividad = worksheet.Cell(currentRow, 1).SetValue<string>("Actividad");
+                    celdaActividad.Style.Font.Bold = true;
+
                     worksheet.Cell(currentRow, 2).SetValue<string>(actividad.actividadCod);
-                    worksheet.Cell(currentRow, 3).SetValue<string>("Dependencia");
-                    worksheet.Cell(currentRow, 4).SetValue<string>("Remuneración");
-                    worksheet.Cell(currentRow, 5).SetValue<string>("Reintegro");
-                    worksheet.Cell(currentRow, 6).SetValue<string>("Deducción");
-                    worksheet.Cell(currentRow, 7).SetValue<string>("Bruto");
-                    worksheet.Cell(currentRow, 8).SetValue<string>("Total Descuento");
-                    worksheet.Cell(currentRow, 9).SetValue<string>("Neto a Pagar");
+
+                    var celdaDependencia = worksheet.Cell(currentRow, 3).SetValue<string>("Dependencia");
+                    celdaDependencia.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+                    celdaDependencia.Style.Font.Bold = true;
+
+                    var celdaRemuneracion = worksheet.Cell(currentRow, 4).SetValue<string>("Remuneración");
+                    celdaRemuneracion.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+                    celdaRemuneracion.Style.Font.Bold = true;
+                    celdaRemuneracion.Style.Border.TopBorder = XLBorderStyleValues.Dashed;
+                    celdaRemuneracion.Style.Border.BottomBorder = XLBorderStyleValues.Dashed;
+
+                    var celdaReintegro = worksheet.Cell(currentRow, 5).SetValue<string>("Reintegro");
+                    celdaReintegro.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+                    celdaReintegro.Style.Font.Bold = true;
+                    celdaReintegro.Style.Border.TopBorder = XLBorderStyleValues.Dashed;
+                    celdaReintegro.Style.Border.BottomBorder = XLBorderStyleValues.Dashed;
+                    
+                    var celdaDeduccion = worksheet.Cell(currentRow, 6).SetValue<string>("Deducción");
+                    celdaDeduccion.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+                    celdaDeduccion.Style.Font.Bold = true;
+                    celdaDeduccion.Style.Border.TopBorder = XLBorderStyleValues.Dashed;
+                    celdaDeduccion.Style.Border.BottomBorder = XLBorderStyleValues.Dashed;
+
+                    var celdaBruto = worksheet.Cell(currentRow, 7).SetValue<string>("Bruto");
+                    celdaBruto.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+                    celdaBruto.Style.Font.Bold = true;
+                    celdaBruto.Style.Border.TopBorder = XLBorderStyleValues.Dashed;
+                    celdaBruto.Style.Border.BottomBorder = XLBorderStyleValues.Dashed;
+                    
+                    var celdaDescuento = worksheet.Cell(currentRow, 8).SetValue<string>("Total Descuento");
+                    celdaDescuento.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+                    celdaDescuento.Style.Font.Bold = true;
+                    celdaDescuento.Style.Border.TopBorder = XLBorderStyleValues.Dashed;
+                    celdaDescuento.Style.Border.BottomBorder = XLBorderStyleValues.Dashed;
+
+                    var celdaSueldo = worksheet.Cell(currentRow, 9).SetValue<string>("Neto a Pagar");
+                    celdaSueldo.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+                    celdaSueldo.Style.Font.Bold = true;
+                    celdaSueldo.Style.Border.TopBorder = XLBorderStyleValues.Dashed;
+                    celdaSueldo.Style.Border.BottomBorder = XLBorderStyleValues.Dashed;
 
                     currentRow++;
 
@@ -136,28 +183,112 @@ namespace Domain.Services.Implementations
                         worksheet.Cell(currentRow, 1).SetValue<string>("");
                         worksheet.Cell(currentRow, 2).SetValue<string>("");
                         worksheet.Cell(currentRow, 3).SetValue<string>(dependencia.dependenciaDesc);
-                        worksheet.Cell(currentRow, 4).SetValue<decimal>(dependencia.totalRemuneracion);
-                        worksheet.Cell(currentRow, 5).SetValue<decimal>(dependencia.totalReintegro);
-                        worksheet.Cell(currentRow, 6).SetValue<decimal>(dependencia.totalDeduccion);
-                        worksheet.Cell(currentRow, 7).SetValue<decimal>(dependencia.totalBruto);
-                        worksheet.Cell(currentRow, 8).SetValue<decimal>(dependencia.totalDescuento);
-                        worksheet.Cell(currentRow, 9).SetValue<decimal>(dependencia.totalSueldo);
+
+                        celdaRemuneracion = worksheet.Cell(currentRow, 4).SetValue<decimal>(dependencia.totalRemuneracion);
+                        celdaRemuneracion.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                        celdaRemuneracion.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+                        celdaReintegro = worksheet.Cell(currentRow, 5).SetValue<decimal>(dependencia.totalReintegro);
+                        celdaReintegro.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+
+                        celdaDeduccion = worksheet.Cell(currentRow, 6).SetValue<decimal>(dependencia.totalDeduccion);
+                        celdaDeduccion.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                        celdaDeduccion.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+                        celdaBruto = worksheet.Cell(currentRow, 7).SetValue<decimal>(dependencia.totalBruto);
+                        celdaBruto.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                        celdaBruto.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+                        celdaDescuento = worksheet.Cell(currentRow, 8).SetValue<decimal>(dependencia.totalDescuento);
+                        celdaDescuento.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                        celdaDescuento.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+                        celdaSueldo = worksheet.Cell(currentRow, 9).SetValue<decimal>(dependencia.totalSueldo);
+                        celdaSueldo.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
 
                         currentRow++;
                     }
 
-                    worksheet.Cell(currentRow, 1).SetValue<string>("Total por Actividad");
+                    celdaActividad = worksheet.Cell(currentRow, 1).SetValue<string>("Total por Actividad");
+                    celdaActividad.Style.Font.Bold = true;
+
                     worksheet.Cell(currentRow, 2).SetValue<string>("");
                     worksheet.Cell(currentRow, 3).SetValue<string>("");
-                    worksheet.Cell(currentRow, 4).SetValue<decimal>(actividad.totalRemuneracion);
-                    worksheet.Cell(currentRow, 5).SetValue<decimal>(actividad.totalReintegro);
-                    worksheet.Cell(currentRow, 6).SetValue<decimal>(actividad.totalDeduccion);
-                    worksheet.Cell(currentRow, 7).SetValue<decimal>(actividad.totalBruto);
-                    worksheet.Cell(currentRow, 8).SetValue<decimal>(actividad.totalDescuento);
-                    worksheet.Cell(currentRow, 9).SetValue<decimal>(actividad.totalSueldo);
+
+                    celdaRemuneracion = worksheet.Cell(currentRow, 4).SetValue<decimal>(actividad.totalRemuneracion);
+                    celdaRemuneracion.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                    celdaRemuneracion.Style.Font.Bold = true;
+                    celdaRemuneracion.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+                    celdaReintegro = worksheet.Cell(currentRow, 5).SetValue<decimal>(actividad.totalReintegro);
+                    celdaReintegro.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                    celdaReintegro.Style.Font.Bold = true;
+
+                    celdaDeduccion = worksheet.Cell(currentRow, 6).SetValue<decimal>(actividad.totalDeduccion);
+                    celdaDeduccion.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                    celdaDeduccion.Style.Font.Bold = true;
+                    celdaDeduccion.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+                    celdaBruto = worksheet.Cell(currentRow, 7).SetValue<decimal>(actividad.totalBruto);
+                    celdaBruto.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                    celdaBruto.Style.Font.Bold = true;
+                    celdaBruto.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+                    celdaDescuento = worksheet.Cell(currentRow, 8).SetValue<decimal>(actividad.totalDescuento);
+                    celdaDescuento.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                    celdaDescuento.Style.Font.Bold = true;
+                    celdaDescuento.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+                    celdaSueldo = worksheet.Cell(currentRow, 9).SetValue<decimal>(actividad.totalSueldo);
+                    celdaSueldo.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                    celdaSueldo.Style.Font.Bold = true;
 
                     currentRow++;
                 }
+
+                worksheet.Cell(currentRow, 1).SetValue<string>("");
+                worksheet.Cell(currentRow, 2).SetValue<string>("");
+                worksheet.Cell(currentRow, 3).SetValue<string>("");
+                
+                var celdaTotalRemuneracion = worksheet.Cell(currentRow, 4).SetValue<decimal>(reporte.totalRemuneracion);
+                celdaTotalRemuneracion.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                celdaTotalRemuneracion.Style.Font.Bold = true;
+                celdaTotalRemuneracion.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                celdaTotalRemuneracion.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                celdaTotalRemuneracion.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+                var celdaTotalReintegro = worksheet.Cell(currentRow, 5).SetValue<decimal>(reporte.totalReintegro);
+                celdaTotalReintegro.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                celdaTotalReintegro.Style.Font.Bold = true;
+                celdaTotalReintegro.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                celdaTotalReintegro.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+
+                var celdaTotalDeduccion = worksheet.Cell(currentRow, 6).SetValue<decimal>(reporte.totalDeduccion);
+                celdaTotalDeduccion.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                celdaTotalDeduccion.Style.Font.Bold = true;
+                celdaTotalDeduccion.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                celdaTotalDeduccion.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                celdaTotalDeduccion.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+                var celdaTotalBruto = worksheet.Cell(currentRow, 7).SetValue<decimal>(reporte.totalBruto);
+                celdaTotalBruto.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                celdaTotalBruto.Style.Font.Bold = true;
+                celdaTotalBruto.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                celdaTotalBruto.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                celdaTotalBruto.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+                var celdaTotalDescuento = worksheet.Cell(currentRow, 8).SetValue<decimal>(reporte.totalDescuento);
+                celdaTotalDescuento.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                celdaTotalDescuento.Style.Font.Bold = true;
+                celdaTotalDescuento.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                celdaTotalDescuento.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+                celdaTotalDescuento.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+
+                var celdaTotalSueldo = worksheet.Cell(currentRow, 9).SetValue<decimal>(reporte.totalSueldo);
+                celdaTotalSueldo.Style.NumberFormat.Format = Formats.BASIC_DECIMAL;
+                celdaTotalSueldo.Style.Font.Bold = true;
+                celdaTotalSueldo.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+                celdaTotalSueldo.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
 
                 using (var stream = new MemoryStream())
                 {
